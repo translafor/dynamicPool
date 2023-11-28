36807f5c773c0092d1aad411c38aa9e2059f6f50
frank.wu <frank.wu@huolala.cn>
2023-11-28 20:21:14 +0800

    feat 项目git初始化

---
+++ src/main/java/com/dynamic/pool/Application.java
@@ -0,0 +1,16 @@
+package com.dynamic.pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: frank.wu
+ * @create: 2023-11-28
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/com/dynamic/pool/ApplicationTest.java
@@ -0,0 +1,19 @@
+package com.dynamic.pool;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @description:
+ * @author: frank.wu
+ * @create: 2023-11-28
+ */
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    public void test() {
+        System.out.println("Spring Boot Test!");
+    }
+
+}
